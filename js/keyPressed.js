a6b92b2867b189d29a50083c857070e3204e659f
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2018-11-06 20:44:01 +0100

    added code
    up/left/down/right functions added for moving the player

--- js/keyPressed.js
+++ js/keyPressed.js
@@ -1,102 +1,108 @@
+function up() {
+  if (lmbWindow === "game") {
+    if (player.gameY >= cellWH) {
+      player.gameY -= cellWH;
+    }
+  } else if (lmbWindow === "GUI") {
+    for (let key in buildings) {
+      // if the lmbBuilding is not at the top of the colomn
+      if ((buildings[lmbBuilding][0] + 1) / maxPreviewRow > 1) {
+        if (buildings[key][0] === buildings[lmbBuilding][0] - maxPreviewRow) {
+          lmbBuilding = key;
+          break;
+        }
+      }
+    }
+
+  }
+
+}
+
+
+function left() {
+  if (lmbWindow === "game") {
+    if (player.gameX >= cellWH + GUIWidth) {
+      player.gameX -= cellWH;
+    }
+  } else if (lmbWindow === "GUI") {
+    for (key in buildings) {
+      // if the lmbBuilding is not at the beginning of the row
+      if ((buildings[lmbBuilding][0]) / maxPreviewRow % 1) {
+        if (buildings[key][0] === buildings[lmbBuilding][0] - 1) {
+          lmbBuilding = key;
+          break;
+        }
+      }
+    }
+  }
+}
+
+
+function down() {
+  if (lmbWindow === "game") {
+    if (player.gameY < height - cellWH - 1) {
+      player.gameY += cellWH;
+    }
+  } else if (lmbWindow === "GUI") {
+    for (key in buildings) {
+      // if the lmbBuilding is not at the bottom of the colomn
+      if ((buildings[lmbBuilding][0] + 1) / maxPreviewRow <= 3) {
+        if (buildings[key][0] === buildings[lmbBuilding][0] + maxPreviewRow) {
+          lmbBuilding = key;
+          break;
+        }
+      }
+    }
+  }
+}
+
+
+function right() {
+  if (lmbWindow === "game") {
+    if (player.gameX < width - cellWH - 1) {
+      player.gameX += cellWH;
+    }
+  } else if (lmbWindow === "GUI") {
+    for (key in buildings) {
+      // if the lmbBuilding is not at the end of the row
+      if ((buildings[lmbBuilding][0] + 1) / maxPreviewRow % 1) {
+        if (buildings[key][0] === buildings[lmbBuilding][0] + 1) {
+          lmbBuilding = key;
+          break;
+        }
+      }
+    }
+  }
+}
+
+
 function keyPressed() {
   switch (keyCode) {
     case 87: // up
-      if (lmbWindow === "game") {
-        if (player.gameY >= cellWH) {
-          player.gameY -= cellWH;
-        }
-      } else if (lmbWindow === "GUI") {
-        for (let key in buildings) {
-          // if the lmbBuilding is not at the top of the colomn
-          if ((buildings[lmbBuilding][0] + 1) / maxPreviewRow > 1) {
-            if (buildings[key][0] === buildings[lmbBuilding][0] - maxPreviewRow) {
-              lmbBuilding = key;
-              break;
-            }
-          }
-        }
-
-      }
+      up()
       break;
     case 65: // left
-      if (lmbWindow === "game") {
-        if (player.gameX >= cellWH + GUIWidth) {
-          player.gameX -= cellWH;
-        }
-      } else if (lmbWindow === "GUI") {
-        for (key in buildings) {
-          // if the lmbBuilding is not at the beginning of the row
-          if ((buildings[lmbBuilding][0]) / maxPreviewRow % 1) {
-            if (buildings[key][0] === buildings[lmbBuilding][0] - 1) {
-              lmbBuilding = key;
-              break;
-            }
-          }
-        }
-      }
+      left()
       break;
     case 83: // down
-      if (lmbWindow === "game") {
-        if (player.gameY < height - cellWH - 1) {
-          player.gameY += cellWH;
-        }
-      } else if (lmbWindow === "GUI") {
-        for (key in buildings) {
-          // if the lmbBuilding is not at the bottom of the colomn
-          if ((buildings[lmbBuilding][0] + 1) / maxPreviewRow <= 3) {
-            if (buildings[key][0] === buildings[lmbBuilding][0] + maxPreviewRow) {
-              lmbBuilding = key;
-              break;
-            }
-          }
-        }
-      }
+      down()
       break;
     case 68: // right
-      if (lmbWindow === "game") {
-        if (player.gameX < width - cellWH - 1) {
-          player.gameX += cellWH;
-        }
-      } else if (lmbWindow === "GUI") {
-        for (key in buildings) {
-          // if the lmbBuilding is not at the end of the row
-          if ((buildings[lmbBuilding][0] + 1) / maxPreviewRow % 1) {
-            if (buildings[key][0] === buildings[lmbBuilding][0] + 1) {
-              lmbBuilding = key;
-              break;
-            }
-          }
-        }
-      }
+      right()
       break;
     case UP_ARROW: // up
-      if (lmbWindow === "game") {
-        if (player.gameY >= cellWH) {
-          player.gameY -= cellWH;
-        }
-      }
+      up()
       break;
     case LEFT_ARROW: // left
-      if (lmbWindow === "game") {
-        if (player.gameX >= cellWH + GUIWidth) {
-          player.gameX -= cellWH;
-        }
-      }
+      left()
       break;
     case DOWN_ARROW: // down
-      if (lmbWindow === "game") {
-        if (player.gameY < height - cellWH - 1) {
-          player.gameY += cellWH;
-        }
-      }
+      down()
       break;
     case RIGHT_ARROW: // right
-      if (lmbWindow === "game") {
-        if (player.gameX < width - cellWH - 1) {
-          player.gameX += cellWH;
-        }
-      }
+      right()
       break;
+
     case 66: // b, buys cells on the right and bottom
       buyLand();
       break;
