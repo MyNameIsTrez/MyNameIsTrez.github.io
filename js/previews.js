9dfaf34ee0c978155a1c50e116c6cd06dba21f14
MyNameIsTrez <welfje@gmail.com>
2018-11-20 13:48:22 +0100

    added old code back

---
+++ js/previews.js
@@ -0,0 +1,54 @@
+class Preview {
+    constructor(building, x, y) {
+        this.building = building;
+        this.x = x;
+        this.y = y + height / 2;
+    }
+    draw() {
+        // if the building is availabe
+        if (buildings[this.building][4]) {
+            if (selectedBuilding === this.building) {
+                noFill();
+                rect(
+                    this.x,
+                    this.y,
+                    previewSize,
+                    previewSize
+                );
+            }
+            image(
+                images[buildings[this.building][0]],
+                this.x, this.y,
+                previewSize,
+                previewSize
+            );
+        }
+    }
+    clicked() {
+        if (
+            (mouseX > this.x) &&
+            (mouseX < (this.x + previewSize)) &&
+            (mouseY > (this.y)) &&
+            (mouseY < (this.y + (previewSize)))
+        ) {
+            selectedBuilding = this.building;
+        }
+    }
+}
+function getPreviewRowsAndColumns() {
+    previewRows = ceil(activePreviews.length / maxPreviewRows);
+    if (activePreviews.length < maxPreviewColumns) {
+        previewColumns = activePreviews.length;
+    } else {
+        previewColumns = maxPreviewColumns;
+    }
+}
+function getActivePreviews() {
+    activePreviews = [];
+    for (key in buildings) {
+        // if the building is availabe
+        if (buildings[key][4]) {
+            activePreviews.push(key);
+        }
+    }
+} 
