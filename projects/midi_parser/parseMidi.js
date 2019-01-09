058cf94f1613851662d31817b781bc2174838210
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-01-09 20:45:01 +0100

    made the function createEvent

--- projects/midi_parser/parseMidi.js
+++ projects/midi_parser/parseMidi.js
@@ -4,8 +4,9 @@ let fs = require(`fs`);
 let midiParser = require(`./midi-parser.js`);
 
 let inputFolder = `./input`;
-// let outputFolder = `./output`;
-let outputFolder = `C:/Users/welfj/AppData/Roaming/.technic/modpacks/tekkit/saves/Creative-2/computer/2`;
+let outputFolder = `./output`;
+// let outputFolder = `C:/Users/welfj/AppData/Roaming/.technic/modpacks/tekkit/saves/Creative-2/computer/2`;
+// let outputFolder = `C:/Users/MML-INFORMATICA/AppData/Roaming/.technic/modpacks/tekkit/saves/Creative-2/computer/2`;
 
 let names = fs.readdirSync(inputFolder);
 
@@ -15,27 +16,22 @@ for (i in names) {
   let name = names[i].replace(/\.[^/.]+$/, ``);
   let data = fs.readFileSync(`./input/` + name + `.mid`, `base64`);
   var midiArray = midiParser.parse(data);
-  let songList = [];
-  let line = -1;
-  let instrumentIndex = 2;
+  var songList = [];
+  var line = 0;
+
+  let instrumentIndex = 2; // changes which instruments play which track
 
   for (track of midiArray.track) { // for every track, max of 5 tracks
-    let instrument = instruments[instrumentIndex++ % instruments.length]; // pick a new instrument
+    var instrument = instruments[instrumentIndex++ % instruments.length]; // pick a new instrument
 
     for (event of track.event) { // for every event
       if (event.type === 9) { // if the event type is `Note On`
-        time = event.deltaTime / 100; // sleep between pitches
-        pitch = Math.round(event.data[0] / (127 / 24)); // pitch 0-127 mapping to pitch 0-24
+        time = event.deltaTime / 20; // sleep between pitches
 
-        if (time > 0) { // play pitches at the same time
-          songList[++line] = [time, [instrument, pitch]]; // create new tone
-        } else {
-          if (line === 0) { // if this is the first line
-            songList[++line] = [40, [instrument, pitch]]; // create new starting tone
-          } else { // 
-            pitch = Math.round(event.data[1] / (127 / 24)); // pitch 0-127 mapping to pitch 0-24
-            songList[line][1].push(pitch) // add extra pitch to tone
-          }
+        if (line === 0) { // if this is the first line, have a delay
+          createEvent(40);
+        } else if (time > 0) { // if this pitch is to be played with a time sleeping afterwards
+          createEvent(time);
         }
       }
     }
@@ -49,7 +45,18 @@ for (i in names) {
   songList = `songList = ` + songList;
 
   // write to file
-  fs.writeFileSync(outputFolder + `/` + name + `.txt`, songList, { spaces: 2, EOL: `\r\n` }, function (err) {
+  fs.writeFileSync(outputFolder + `/` + name + `.json`, songList, { spaces: 2, EOL: `\r\n` }, function (err) {
     if (err) console.error(err);
   })
+}
+
+function createEvent(jeff) {
+  songList[line] = [jeff, [instrument]]; // create new event
+
+  for (i in event.data) { // an 'in' loop gets you the index of an array, 'of' gets you the elements in the array
+    pitch = Math.round(event.data[i] / (127 / 24)); // pitch 0-127 mapping to pitch 0-24
+    songList[line][1].push(pitch) // add extra pitch to tone
+  }
+
+  line++;
 }
