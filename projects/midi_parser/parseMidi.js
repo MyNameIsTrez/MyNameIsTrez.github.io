17d857b19a9c766845e3e71f2a0561a1666638fa
MyNameIsTrez <welfje@gmail.com>
2019-01-10 14:22:04 +0100

    Now properly interlaces the tracks.

--- projects/midi_parser/parseMidi.js
+++ projects/midi_parser/parseMidi.js
@@ -72,40 +72,46 @@ for (let name in names) {
 
 
 
-  // interlace the tracks
-  console.log(tracksEventCount[0])
+  // interlaces the tracks
+  // console.log(tracksEventCount[0]);
   for (let i = 0; i < tracksEventCount[0]; i++) { // loop the largest amount of note events a track has times
     for (index = 0; index < trackIndexes.length; index++) { // loop the length of trackIndexes, 5, times
-      let track = midiArray.track[trackIndexes[index]]; // pick a new track 
-      let instrument = instruments[index]; // pick a new instrument that is based on the track number
-      for (let event of track.event) { // for every event
-        if (event.type === 9) { // if the event type is `Note On`
-          let time = Math.round(event.deltaTime / 20); // the sleep time between now and the next note event
-          if (line === 0) {
-            createEvent(event, 40, instrument);
-          } else {
-            createEvent(event, time, instrument);
-          }
+      let track = midiArray.track[trackIndexes[index]]; // pick a new track from the largest to the smallest
+      let instrument = instruments[index]; // pick a new instrument that is based on the track index
+      checkIfNote(track, instrument);
+    }
+  }
+
+
+
+  function checkIfNote(track, instrument) { // returns out of this loop when the next track can be played
+    for (let event of track.event) { // for every event
+      if (event.type === 9) { // if the event type is `Note On`
+        let time = Math.round(event.deltaTime / 20); // the sleep time between now and the next note event
+        if (line === 0) {
+          createEvent(event, 40, instrument);
+        } else {
+          createEvent(event, time, instrument);
         }
+        return // returns out of this loop so the next track can be played
       }
     }
   }
 
 
 
-  console.log(`Parsing...`)
   // create a new note event or add onto an existing one
+  console.log(`Parsing...`)
+
   function createEvent(event, time, instrument) {
     if (time > 0) {
       if (lineOne === true) {
         line++;
         lineOne = false;
-        // console.log(`lineOne === true -> false`);
       }
       if (line === 0) { // if this is the first line, don't skip to the next line
         songList[line] = [time, [instrument]]; // create new event at line 0 with a delay
         lineOne = true;
-        // console.log(`line === 0, songList: ${songList}.`);
       } else {
         songList[++line] = [time, [instrument]]; // create new event
       }
@@ -113,9 +119,6 @@ for (let name in names) {
 
     for (let index in event.data) { // an 'in' loop gets you the index of an array, 'of' gets you the elements in the array
       pitch = Math.round(event.data[index] / (127 / 24)); // pitch 0-127 mapping to pitch 0-24
-      if (line % 10000 === 0) {
-        console.log(`line: ${line / 1000000} million`);
-      }
       songList[line][1].push(pitch) // add extra pitch to tone
     }
   }
