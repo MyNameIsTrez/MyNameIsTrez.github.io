28d62676333ac8b1ae6b25b73e0dc6ca37cc8534
MyNameIsTrez <welfje@gmail.com>
2019-01-16 15:15:23 +0100

    finished parseMidi.js

--- projects/midi_parser/parseMidi.js
+++ projects/midi_parser/parseMidi.js
@@ -1,4 +1,4 @@
-// use `node parseMidi.js` in this folder to execute
+// use `node parseMidi.js` with node in this folder to execute
 
 /*
 * TODO:
@@ -8,58 +8,62 @@
   [[ 0, [`snare`, 3, 5]], [10, [`harp`, 4, 18]], [ 0, [`bassdrum`, 7, 15]]] play track 0 and 2's notes, because element 0 and 2 are both a time of 0
   [[ 0, [`snare`, 3, 5]], [10, [`harp`, 4, 18]], [ 0, [`bassdrum`, 7, 15]]] sleep 10, because you just removed that amount
   [[35, [`bass`, 16, 8]], [10, [`harp`, 4, 18]], [20, [`hat`, 18, 5]]] make a new element for track 0 and 2, of note event 1
+* Sort the tracks from largest to smallest in the output JSON.
 * Currently, the CC Lua program can only read multiple pitches of the same instrument
   at the same time in the same 'object' if it's pitches are either 0-15 or 16-24.
   I need to add the ability to play pitches from 0-24 in the same 'object'.
 */
 
 // uneditable setup variables
-let fs = require(`fs`);
-let midiParser = require(`./midi-parser.js`);
+const fs = require(`fs`);
+const midiParser = require(`./midi-parser.js`);
 
-let inputFolder = `./input/`;
-let names = fs.readdirSync(inputFolder);
+const inputFolder = `./input/`;
+const names = fs.readdirSync(inputFolder);
+const instruments = [`bass`, `snare`, `hat`, `bassdrum`, `harp`];
 
 // The `output` folder
-let outputFolder = `./output/`;
+// const outputFolder = `./output/`;
 // Home PC Tekkit Classic folder
-// let outputFolder = `C:/Users/welfj/AppData/Roaming/.technic/modpacks/tekkit/saves/Creative-2/computer/2`;
+const outputFolder = `C:/Users/welfj/AppData/Roaming/.technic/modpacks/tekkit/saves/Creative-2/computer/2/`;
 // School Laptop Tekkit Classic folder
-// let outputFolder = `C:/Users/MML-INFORMATICA/AppData/Roaming/.technic/modpacks/tekkit/saves/Creative-2/computer/2`;
+// const outputFolder = `C:/Users/MML-INFORMATICA/AppData/Roaming/.technic/modpacks/tekkit/saves/Creative-2/computer/2/`;
 
-let instruments = [`bass`, `snare`, `hat`, `bassdrum`, `harp`];
+const nps = 5; // the amount of times a single noteblock can make a sound in a second
 
 for (let name in names) {
   name = names[name].replace(/\.[^/.]+$/, ``);
-  let data = fs.readFileSync(inputFolder + name + `.mid`, `base64`);
-  let midiArray = midiParser.parse(data);
+  const data = fs.readFileSync(inputFolder + name + `.mid`, `base64`);
+  let midi = midiParser.parse(data);
 
-  var songList = [];
-  var line = 0;
-  var lineOne = false;
+  // delete all the keys that aren't part of the track
+  Object.keys(midi).forEach(key => {
+    if (key !== `track`) {
+      delete midi[key];
+    }
+  });
 
   // filter out events that aren't of type 9
-  for (const track of midiArray.track) {
+  for (const track of midi.track) {
     track.event = track.event.filter(event => {
-      // console.log(event.type === 9);
       return event.type === 9
     });
   }
 
-  // top 5 of how many note events each track has
-  var unsortedTracksNoteCount;
+  // get the top 5 of how many note events each track has
+  let unsortedTracksNoteCount;
   var tracksNoteCount = getTracksNoteCount();
-  // indexes of the tracks of tracksNoteCount
+  // get the indexes of the tracks of tracksNoteCount
   var trackIndexes = getTrackIndexes();
-  // console.log(unsortedTracksNoteCount, tracksNoteCount, trackIndexes);
 
   function getTracksNoteCount() {
     tracksNoteCount = [];
-    // for every track, max of 5 tracks with 5 noteblock instruments
-    for (let track of midiArray.track) {
-      tracksNoteCount[midiArray.track.indexOf(track)] = 0;
+
+    // calculates the number of note events a track has
+    for (let track of midi.track) {
+      tracksNoteCount[midi.track.indexOf(track)] = 0;
       for (let event of track.event) {
-        tracksNoteCount[midiArray.track.indexOf(track)]++;
+        tracksNoteCount[midi.track.indexOf(track)]++;
       }
     }
 
@@ -80,69 +84,46 @@ for (let name in names) {
     return trackIndexes;
   }
 
-  midiArray.track = midiArray.track.filter(track => {
-    return trackIndexes.includes(midiArray.track.indexOf(track));
+  // remove the tracks that aren't part of the top 5 tracks
+  midi.track = midi.track.filter(track => {
+    return trackIndexes.includes(midi.track.indexOf(track));
   });
 
-  // write to file
-  fs.writeFileSync(outputFolder + name + 'Midi' + '.json', JSON.stringify(midiArray), {
-    spaces: 2,
-    EOL: '\r\n'
-  }, function (err) {
-    if (err) console.error(err);
-  })
+  // replaces `{tracks: {event: [note events]}, {event: [note events]}}` with `[note events], [note events]`
+  // and puts the result in the new constant, midiArray
+  let midiArray = [];
+  for (let i = 0; i < midi.track.length; i++) {
+    midiArray.push(midi.track[i].event);
+  }
 
-  // create a new note event or add onto an existing one
-  function createEvent(event, time, instrument) {
-    if (line === 0) {
-      time = 40;
-    }
-
-    if (time > 0) {
-      if (lineOne === true) {
-        line++;
-        lineOne = false;
+  // replaces `[{deltatime:, data:}, {deltatime:, data:}], [{deltatime:, data:}]`
+  // with `[[deltatime:, data:], [deltatime:, data:]], [[deltatime:, data:]]`
+  for (let i = 0; i < midiArray.length; i++) {
+    for (let j = 0; j < midiArray[i].length; j++) {
+      let data = midiArray[i][j].data;
+      midiArray[i][j] = [Math.round(midiArray[i][j].deltaTime / nps), [instruments[i]]];
+      for (let k = 0; k < data.length; k++) {
+        midiArray[i][j][1].push(Math.round(data[k] * (24 / 127)));
       }
-      // if this is the first line, don't skip to the next line
-      if (line === 0) {
-        // create a new event at line 0 with a delay
-        songList[line] = [time, [instrument]];
-        lineOne = true;
-      } else {
-        // create a new event
-        songList[++line] = [time, [instrument]];
-      }
-    }
-
-    for (let index in event.data) {
-      // pitch 0-127 to pitch 0-24
-      // pitch = Math.round(event.data[index] * (24 / 127));
-
-      // pitch 0-127 to pitch 0-24
-      pitch = Math.round(event.data[index]);
-
-      // add extra pitch to tone
-      songList[line][1].push(pitch)
     }
   }
 
   // final parsing details
-  // replace `[` with `{`
-  songList = JSON.stringify(songList).replace(/\[/g, `{`);
-  // replace `]` with `}`
-  songList = JSON.stringify(songList).replace(/\]/g, `}`);
-  // remove `\`
-  songList = songList.replace(/\\/g, ``);
-  songList = songList.substring(1, songList.length - 1);
-  // add `songList = ` to the beginning of the songList
-  songList = `songList = ` + songList;
+  // replaces `[` with `{`
+  midiArray = JSON.stringify(midiArray).replace(/\[/g, `{`);
+  // replaces `]` with `}`
+  midiArray = JSON.stringify(midiArray).replace(/\]/g, `}`);
+  // removes `\`
+  midiArray = midiArray.replace(/\\/g, ``);
+  midiArray = midiArray.substring(1, midiArray.length - 1);
+  // adds `midiArray = ` to the beginning of the midiArray
+  midiArray = `midiArray = ` + midiArray;
 
   // write to file
-  fs.writeFileSync(outputFolder + name + `.json`, songList, {
+  fs.writeFileSync(outputFolder + name + '.json', midiArray, {
     spaces: 2,
-    EOL: `\r\n`
+    EOL: '\r\n'
   }, function (err) {
     if (err) console.error(err);
-  })
-  console.log(`Done!`)
+  });
 }
