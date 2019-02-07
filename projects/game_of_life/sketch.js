0005c18d9843298498c2415c5c7e9cd66dac66f6
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-02-07 02:20:39 +0100

    Made more progress with cells refactoring.

--- projects/game_of_life/sketch.js
+++ projects/game_of_life/sketch.js
@@ -22,15 +22,15 @@ let cursor_color = [0, 127, 0]; // the cursor color
 
 const saves = {
   // name: [cell_tick_rate, cell_width_height, cell_width_count, cell_height_count, first cell-alive state, [length of cells with the same cell-alive states]]
-  blinker: [3, 150, 5, 5, 0, [11, 3]],
-  toad: [3, 125, 6, 6, 0, [14, 3, 2, 3]],
-  beacon: [3, 125, 6, 6, 0, [7, 2, 4, 2, 6, 2, 4, 2]],
-  pulsar: [3, 45, 17, 17, 0, [38, 3, 3, 3, 23, 1, 4, 1, 1, 1, 4, 1, 4, 1, 4, 1, 1, 1, 4, 1, 4, 1, 4, 1, 1, 1, 4, 1, 6, 3, 3, 3, 25, 3, 3, 3, 6, 1, 4, 1, 1, 1, 4, 1, 4, 1, 4, 1, 1, 1, 4, 1, 4, 1, 4, 1, 1, 1, 4, 1, 23, 3, 3, 3]],
-  r_pentomino: [60, 8, 100, 100, 0, [4952, 2, 97, 2, 99, 1]],
-  glider: [6, 80, 10, 10, 0, [13, 1, 7, 1, 1, 1, 8, 2]],
-  gosper_glider_gun: [30, 16, 38, 49, 0, [63, 1, 35, 1, 1, 1, 25, 2, 6, 2, 12, 2, 13, 1, 3, 1, 4, 2, 12, 2, 2, 2, 8, 1, 5, 1, 3, 2, 16, 2, 8, 1, 3, 1, 1, 2, 4, 1, 1, 1, 23, 1, 5, 1, 7, 1, 24, 1, 3, 1, 34, 2, 117, 2, 36, 2, 376, 2, 3, 2, 70, 1, 3, 1, 34, 3, 35, 3, 114, 1, 36, 3, 34, 1, 3, 1, 35, 1, 34, 1, 5, 1, 31, 1, 5, 1, 32, 1, 3, 1, 34, 3, 339, 2, 36, 2]],
-  acorn: [60, 5, 150, 150, 0, [11196, 1, 151, 1, 146, 2, 2, 3]],
-  stick: [60, 5, 150, 150, 0, [11305, 8, 1, 5, 3, 3, 6, 7, 1, 5]]
+  // blinker: [3, 150, 5, 5, 0, [11, 3]],
+  // toad: [3, 125, 6, 6, 0, [14, 3, 2, 3]],
+  // beacon: [3, 125, 6, 6, 0, [7, 2, 4, 2, 6, 2, 4, 2]],
+  // pulsar: [3, 45, 17, 17, 0, [38, 3, 3, 3, 23, 1, 4, 1, 1, 1, 4, 1, 4, 1, 4, 1, 1, 1, 4, 1, 4, 1, 4, 1, 1, 1, 4, 1, 6, 3, 3, 3, 25, 3, 3, 3, 6, 1, 4, 1, 1, 1, 4, 1, 4, 1, 4, 1, 1, 1, 4, 1, 4, 1, 4, 1, 1, 1, 4, 1, 23, 3, 3, 3]],
+  // r_pentomino: [60, 8, 100, 100, 0, [4952, 2, 97, 2, 99, 1]],
+  // glider: [6, 80, 10, 10, 0, [13, 1, 7, 1, 1, 1, 8, 2]],
+  // gosper_glider_gun: [30, 16, 38, 49, 0, [63, 1, 35, 1, 1, 1, 25, 2, 6, 2, 12, 2, 13, 1, 3, 1, 4, 2, 12, 2, 2, 2, 8, 1, 5, 1, 3, 2, 16, 2, 8, 1, 3, 1, 1, 2, 4, 1, 1, 1, 23, 1, 5, 1, 7, 1, 24, 1, 3, 1, 34, 2, 117, 2, 36, 2, 376, 2, 3, 2, 70, 1, 3, 1, 34, 3, 35, 3, 114, 1, 36, 3, 34, 1, 3, 1, 35, 1, 34, 1, 5, 1, 31, 1, 5, 1, 32, 1, 3, 1, 34, 3, 339, 2, 36, 2]],
+  // acorn: [60, 5, 150, 150, 0, [11196, 1, 151, 1, 146, 2, 2, 3]],
+  // stick: [60, 5, 150, 150, 0, [11305, 8, 1, 5, 3, 3, 6, 7, 1, 5]]
 }
 
 const settings = [`clear cells`, `loop edges: `, `draw grid paused: `, `draw grid playing: `, `game mode: `, `cell tick rate: `, `cell width & height: `, `cell width count: `, `cell height count: `];
@@ -67,6 +67,7 @@ let
   rect_text_space;
 
 function createGame() {
+  playing = false;
   cells = []; // removes all cells, for when you 
   game_width = cell_width_height * cell_width_count;
   game_height = cell_width_height * cell_height_count;
@@ -83,6 +84,9 @@ function createGame() {
       cells[y].push(cell);
     }
   }
+
+  cursor.x = 0;
+  cursor.y = 0;
 
   input_save.position(game_width / 2 - input_save.width / 2 - 83 / 2, canvas_height + 15 + 25);
   button_save.position(input_save.x + input_save.width + 5, input_save.y);
@@ -297,34 +301,23 @@ function load_game(save_number) {
   cell_width_count = saves[save_name][2];
   cell_height_count = saves[save_name][3];
 
-  game_width = cell_width_height * cell_width_count;
-  game_height = cell_width_height * cell_height_count;
-  canvas_height = game_height + 100;
-  playing = false;
-  cells = [];
-  createCanvas(game_width + 1, canvas_height + 1); // `+ 1` is needed to show the bottom and right strokes
-
-  for (let y = 0; y < cell_height_count; y++) {
-    for (let x = 0; x < cell_width_count; x++) {
-      const cell = new Cell(x * cell_width_height, y * cell_width_height, cells.length);
-      cells.push(cell)
-    }
-  }
+  createGame();
 
   let alive = saves[save_name][4]; // the starting cell`s alive state
-  let cell = 0;
+  let cell_x = 0;
+  let cell_y = 0;
+
   for (const size of saves[save_name][5]) {
     for (let i = 0; i < size; i++) {
-      cells[cell++].alive = alive ? 1 : 0;
+      if (cell_x < cell_width_count) {
+        cells[cell_y][cell_x++].alive = alive;
+      } else {
+        cell_x = 0;
+        cell_y++;
+      }
     }
-    alive = !alive;
+    alive = !alive ? 1 : 0;
   }
-
-  input_save.position(game_width / 2 - input_save.width / 2 - 83 / 2, canvas_height + 15 + 25);
-  button_save.position(input_save.x + input_save.width + 5, input_save.y);
-
-  cursor.x = 0;
-  cursor.y = 0;
 
   screen = `game`;
 }
@@ -336,20 +329,46 @@ function save_game() {
   if (input_save.value() in saves) {
     throw `Error: A save with that name already exists.`;
   }
-  let aliveCells = [];
+
   // push the game's settings and the cell-alive state of the first cell
+  let aliveCells = [];
   aliveCells.push(cell_tick_rate, cell_width_height, cell_width_count, cell_height_count, cells[0].alive, []);
+
   let length = 1;
-  for (let cell in cells) {
-    if (cell >= 1) {
-      if (cells[cell].alive === cells[cell - 1].alive) {
-        length++;
-      } else {
-        aliveCells[5].push(length);
-        length = 1;
+  for (let y in cells) {
+    for (let x in cells[y]) {
+      if (x = 0 && y > 0) {
+        if (cells[y][x].alive === cells[y - 1][cell_width_count - 1].alive) {
+          length++;
+        } else {
+          aliveCells[5].push(length);
+          length = 1;
+        }
+      }
+
+      if (x >= 1) {
+        if (cells[y][x].alive === cells[y][x - 1].alive) {
+          length++;
+        } else {
+          aliveCells[5].push(length);
+          length = 1;
+        }
       }
     }
   }
+
+  // let length = 1;
+  // for (let cell in cells) {
+  //   if (cell >= 1) {
+  //     if (cells[cell].alive === cells[cell - 1].alive) {
+  //       length++;
+  //     } else {
+  //       aliveCells[5].push(length);
+  //       length = 1;
+  //     }
+  //   }
+  // }
+
   console.log(input_save.value() + `:`, JSON.stringify(aliveCells));
   saves[input_save.value()] = aliveCells;
   localStorage.setItem(`GOL_saves`, JSON.stringify(saves));
