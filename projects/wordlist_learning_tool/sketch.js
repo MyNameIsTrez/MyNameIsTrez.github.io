8bf7f344251077c271ca345a2115abdd09e144bd
MyNameIsTrez <welfje@gmail.com>
2019-03-14 23:31:35 +0100

    Added solarized as a bg color, made the text white and added a progress counter.

--- projects/wordlist_learning_tool/sketch.js
+++ projects/wordlist_learning_tool/sketch.js
@@ -1,18 +1,26 @@
 let index = 0;
-let countDiv;
+let countDiv, progressDiv;
 
 function setup() {
+  document.body.style.backgroundColor = '#073642';
   countDiv = createDiv(words[index]);
   countDiv.style('font-size', '100pt');
+  countDiv.style('color', 'white');
+  
+  progressDiv = createDiv(`${(index + 1)}/${words.length}`);
+  progressDiv.style('font-size', '100pt');
+  progressDiv.style('color', 'white');
 }
 
 function mousePressed() {
   if (mouseButton === RIGHT) { // repeat the words later
     words.push(words[index]);
+    progressDiv.html(`${(index + 1)}/${words.length}`);
   }
   if (index < words.length - 1) {
     index++;
     countDiv.html(words[index]);
+    progressDiv.html(`${(index + 1)}/${words.length}`);
   } else {
     countDiv.html("You're done!");
     exit;
