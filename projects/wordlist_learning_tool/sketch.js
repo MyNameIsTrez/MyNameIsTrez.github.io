d04939376e4ffa26da31fe933256c06903d9e867
MyNameIsTrez <welfje@gmail.com>
2019-03-15 15:00:12 +0100

    Now using html instead of p5 for text!

--- projects/wordlist_learning_tool/sketch.js
+++ projects/wordlist_learning_tool/sketch.js
@@ -2,46 +2,32 @@ let wordsExist = false;
 let words = [];
 let index = 0;
 let difficultWords = [];
-let countDiv, progressDiv;
 let released = true;
 
 function setup() {
   google.charts.load('current', {packages: ['corechart']});
   google.charts.setOnLoadCallback(getData);
-
-  countDiv = createDiv(words[index]);
-  countDiv.style('font-size', '100pt');
-  countDiv.style('color', 'white');
-  
-  progressDiv = createDiv('Loading...');
-  progressDiv.style('font-size', '100pt');
-  progressDiv.style('color', 'white');
 }
 
 function getData() {
-    console.log('started getData()');
-    // https://docs.google.com/spreadsheets/d/1t3TuSANJlhotUmUpOIBQNYyZMU6rmNSAOge-KkssOpQ/edit?usp=sharing
-    let SPREADSHEET_URL = 'https://docs.google.com/a/google.com/spreadsheets/d/1t3TuSANJlhotUmUpOIBQNYyZMU6rmNSAOge-KkssOpQ/gviz/tq?tq=select%20A';
-    let query = new google.visualization.Query(SPREADSHEET_URL);
-    query.setQuery('select A');
-    query.send(handleQueryResponse);
-    console.log('finished getData()');
+  let SPREADSHEET_URL = 'https://docs.google.com/a/google.com/spreadsheets/d/1t3TuSANJlhotUmUpOIBQNYyZMU6rmNSAOge-KkssOpQ/gviz/tq?tq=select%20A';
+  let query = new google.visualization.Query(SPREADSHEET_URL);
+  query.setQuery('select *');
+  query.send(handleQueryResponse);
 }
 
 function handleQueryResponse(response) {
-    console.log('started handleQueryResponse(response)');
-    let data = response.getDataTable();
-    console.log('finished handleQueryResponse(response)');
-    dataIntoWordArray(data);
+  let data = response.getDataTable();
+  dataIntoWordArray(data);
 }
 
 function dataIntoWordArray(data) {
-    for (let i = 0; i < data.wg.length; i++) {
-        words[i] = data.wg[i].c[0].v;
-    }
-    wordsExist = true;
-    countDiv.html(words[index]);
-    progressDiv.html(`${(index + 1)}/${words.length}`);
+  for (let i = 0; i < data.wg.length; i++) {
+    words[i] = data.wg[i].c[0].v;
+  }
+  wordsExist = true;
+  document.getElementById("word").innerHTML = words[index];
+  document.getElementById("progress").innerHTML = `${(index + 1)}/${words.length}`;
 }
 
 function mouseReleased(){
@@ -61,15 +47,15 @@ function mousePressed() {
     if (mouseButton === RIGHT) { // repeat the words later
       words.push(words[index]);
       difficultWords.push(words[index]);
-      progressDiv.html(`${(index + 1)}/${words.length}`);
+      document.getElementById("progress").innerHTML = `${(index + 1)}/${words.length}`;
     }
 
     if (index < words.length - 1) {
       index++;
-      countDiv.html(words[index]);
-      progressDiv.html(`${(index + 1)}/${words.length}`);
+      document.getElementById("word").innerHTML = words[index];
+      document.getElementById("progress").innerHTML = `${(index + 1)}/${words.length}`;
     } else {
-      countDiv.html('You\'re done!');
+      document.getElementById("word").innerHTML = 'You\'re done!';
       exit;
     }
   }
