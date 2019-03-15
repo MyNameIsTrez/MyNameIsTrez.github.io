1a0ef75f7d5057c605efe75ed65cefe9b8ce5947
MyNameIsTrez <welfje@gmail.com>
2019-03-15 13:39:03 +0100

    Working!

--- projects/wordlist_learning_tool/sketch.js
+++ projects/wordlist_learning_tool/sketch.js
@@ -1,50 +1,77 @@
+let wordsExist = false;
+let words = [];
 let index = 0;
-let countDiv, progressDiv;
 let difficultWords = [];
-
-words = words.split(' ');
-for (let i = 0; i < words.length; i++) {
-  const regex = /_/gi;
-  words[i] = words[i].replace(regex, ' ');
-}
+let countDiv, progressDiv;
+let released = true;
 
 function setup() {
-  document.body.style.backgroundColor = '#073642';
+  google.charts.load('current', {packages: ['corechart']});
+  google.charts.setOnLoadCallback(getData);
+
   countDiv = createDiv(words[index]);
   countDiv.style('font-size', '100pt');
   countDiv.style('color', 'white');
   
-  progressDiv = createDiv(`${(index + 1)}/${words.length}`);
+  progressDiv = createDiv('Loading...');
   progressDiv.style('font-size', '100pt');
   progressDiv.style('color', 'white');
 }
 
-let released = true;
+function getData() {
+    console.log('started getData()');
+    // https://docs.google.com/spreadsheets/d/1t3TuSANJlhotUmUpOIBQNYyZMU6rmNSAOge-KkssOpQ/edit?usp=sharing
+    let SPREADSHEET_URL = 'https://docs.google.com/a/google.com/spreadsheets/d/1t3TuSANJlhotUmUpOIBQNYyZMU6rmNSAOge-KkssOpQ/gviz/tq?tq=select%20A';
+    let query = new google.visualization.Query(SPREADSHEET_URL);
+    query.setQuery('select A');
+    query.send(handleQueryResponse);
+    console.log('finished getData()');
+}
+
+function handleQueryResponse(response) {
+    console.log('started handleQueryResponse(response)');
+    let data = response.getDataTable();
+    console.log('finished handleQueryResponse(response)');
+    dataIntoWordArray(data);
+}
+
+function dataIntoWordArray(data) {
+    for (let i = 0; i < data.wg.length; i++) {
+        words[i] = data.wg[i].c[0].v;
+    }
+    wordsExist = true;
+    countDiv.html(words[index]);
+    progressDiv.html(`${(index + 1)}/${words.length}`);
+}
 
 function mouseReleased(){
-	released = true;
-	return false;
+  if (wordsExist) {
+    released = true;
+    return false;
+  }
 }
 
 function mousePressed() {
-	if(!released){
-		return;
-	}
-  released = false;
-  
-  if (mouseButton === RIGHT) { // repeat the words later
-    words.push(words[index]);
-    difficultWords.push(words[index]);
-    progressDiv.html(`${(index + 1)}/${words.length}`);
-  }
+  if (wordsExist) {
+    if(!released){
+      return;
+    }
+    released = false;
+    
+    if (mouseButton === RIGHT) { // repeat the words later
+      words.push(words[index]);
+      difficultWords.push(words[index]);
+      progressDiv.html(`${(index + 1)}/${words.length}`);
+    }
 
-  if (index < words.length - 1) {
-    index++;
-    countDiv.html(words[index]);
-    progressDiv.html(`${(index + 1)}/${words.length}`);
-  } else {
-    countDiv.html('You\'re done!');
-    exit;
+    if (index < words.length - 1) {
+      index++;
+      countDiv.html(words[index]);
+      progressDiv.html(`${(index + 1)}/${words.length}`);
+    } else {
+      countDiv.html('You\'re done!');
+      exit;
+    }
   }
 }
 
