ba66ad26a15d7cd9de7d52fa538c0137acdc43b2
MyNameIsTrez <welfje@gmail.com>
2019-03-16 23:57:23 +0100

    Logging which mouseButton is pressed.

--- projects/wordlist_learning_tool/sketch.js
+++ projects/wordlist_learning_tool/sketch.js
@@ -1,8 +1,9 @@
 let wordsExist = false;
-let words = [];
+let words = { 1: [], 2: [], 3: []};
 let index = 0;
-let difficultWords = [];
 let released = true;
+let showAnswer = true;
+let done = false;
 
 function setup() {
   google.charts.load('current', {packages: ['corechart']});
@@ -10,24 +11,42 @@ function setup() {
 }
 
 function getData() {
-  const spreadsheetFilter = '/gviz/tq?tq=select%20A' // select B
-  const query = new google.visualization.Query(spreadsheetURL + spreadsheetFilter);
-  query.setQuery('select B');
-  query.send(handleQueryResponse);
+  // let spreadsheetFilter = 'select%20B%2C%20offset%201' // select B, offset 1
+
+  let spreadsheetFilter = 'select%20B' // select B
+  let query = new google.visualization.Query(spreadsheetURL + '/gviz/tq?tq=' + spreadsheetFilter);
+  query.send(handleQueryResponse1);
+  
+  spreadsheetFilter = 'select%20D' // select D
+  query = new google.visualization.Query(spreadsheetURL + '/gviz/tq?tq=' + spreadsheetFilter);
+  query.send(handleQueryResponse2);
 }
 
-function handleQueryResponse(response) {
+function handleQueryResponse1(response) {
   const data = response.getDataTable();
-  dataIntoWordArray(data);
+  dataIntoWordArray1(data);
 }
 
-function dataIntoWordArray(data) {
+function handleQueryResponse2(response) {
+  const data = response.getDataTable();
+  dataIntoWordArray2(data);
+}
+
+function dataIntoWordArray1(data) {
   for (let i = 0; i < data.wg.length; i++) {
-    words[i] = data.wg[i].c[0].v;
+    words[1][i] = data.wg[i].c[0].v;
   }
+
+  document.getElementById('words1').innerHTML = words[1][index];
+  document.getElementById('progress').innerHTML = `${(index + 1)}/${words[1].length}`;
+}
+
+function dataIntoWordArray2(data) {
+  for (let i = 0; i < data.wg.length; i++) {
+    words[2][i] = data.wg[i].c[0].v;
+  }
+
   wordsExist = true;
-  document.getElementById("word").innerHTML = words[index];
-  document.getElementById("progress").innerHTML = `${(index + 1)}/${words.length}`;
 }
 
 function mouseReleased(){
@@ -44,18 +63,52 @@ function mousePressed() {
     }
     released = false;
     
-    if (mouseButton === RIGHT) { // repeat the words later
-      words.push(words[index]);
-      difficultWords.push(words[index]);
-      document.getElementById("progress").innerHTML = `${(index + 1)}/${words.length}`;
+    if (index >= words[1].length) {
+      document.getElementById("words1").innerHTML = 'You\'re done!';
+      document.getElementById("words2").innerHTML = '';
+      done = true;
     }
 
-    if (index < words.length - 1) {
-      index++;
-      document.getElementById("word").innerHTML = words[index];
-      document.getElementById("progress").innerHTML = `${(index + 1)}/${words.length}`;
-    } else {
-      document.getElementById("word").innerHTML = 'You\'re done!';
+    if (done) return;
+
+    console.log(mouseButton);
+
+    switch(mouseButton) {
+      case RIGHT:
+        if (!showAnswer) {
+          document.getElementById("words1").innerHTML = words[1][index];
+          document.getElementById("words2").innerHTML = '';
+          document.getElementById("progress").innerHTML = `${(index + 1)}/${words[1].length}`;
+
+          words[1].push(words[1][index - 1]);
+          words[3][index - 1] = words[1][index - 1];
+
+          showAnswer = true;
+        } else {        
+          document.getElementById("words2").innerHTML = words[2][index];
+          document.getElementById("progress").innerHTML = `${(index + 1)}/${words[1].length}`;
+
+          words[1].push(words[1][index]);
+          words[3][index] = words[1][index];
+
+          index++;
+          showAnswer = false;
+        }
+        break;
+      case LEFT:
+        if (!showAnswer) {
+          document.getElementById("words1").innerHTML = words[1][index];
+          document.getElementById("words2").innerHTML = '';
+          document.getElementById("progress").innerHTML = `${(index + 1)}/${words[1].length}`;
+
+          showAnswer = true;
+        } else {        
+          document.getElementById("words2").innerHTML = words[2][index];
+
+          index++;
+          showAnswer = false;
+        }
+        break;
     }
   }
 }
