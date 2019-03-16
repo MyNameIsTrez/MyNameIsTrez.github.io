a8f47cd372b0762236759b58c33d49173805fc5e
MyNameIsTrez <welfje@gmail.com>
2019-03-16 23:41:20 +0100

    Removed an empty folder.

--- projects/wordlist_learning_tool/sketch.js
+++ projects/wordlist_learning_tool/sketch.js
@@ -15,12 +15,10 @@ function getData() {
 
   let spreadsheetFilter = 'select%20B' // select B
   let query = new google.visualization.Query(spreadsheetURL + '/gviz/tq?tq=' + spreadsheetFilter);
-  query.setQuery('select B');
   query.send(handleQueryResponse1);
   
   spreadsheetFilter = 'select%20D' // select D
   query = new google.visualization.Query(spreadsheetURL + '/gviz/tq?tq=' + spreadsheetFilter);
-  query.setQuery('select D');
   query.send(handleQueryResponse2);
 }
 
