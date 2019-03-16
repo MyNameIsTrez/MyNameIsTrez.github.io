c10f13a27a14f07c371abb9401a7799a9a4c8cdf
MyNameIsTrez <welfje@gmail.com>
2019-03-16 17:47:00 +0100

    Made words german.

--- projects/wordlist_learning_tool/spreadsheetURL.js
+++ projects/wordlist_learning_tool/spreadsheetURL.js
@@ -1,5 +1,5 @@
 // German Leiden 1-10
-// const spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1mzS7qgvcLEctfhm4p8welUXpeE9DyKjBFz14ZvYG_30';
+const spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1mzS7qgvcLEctfhm4p8welUXpeE9DyKjBFz14ZvYG_30';
 
 // English 5V P2 toetsweek
-const spreadsheetURL = 'https://docs.google.com/a/google.com/spreadsheets/d/1t3TuSANJlhotUmUpOIBQNYyZMU6rmNSAOge-KkssOpQ';
+// const spreadsheetURL = 'https://docs.google.com/a/google.com/spreadsheets/d/1t3TuSANJlhotUmUpOIBQNYyZMU6rmNSAOge-KkssOpQ';
