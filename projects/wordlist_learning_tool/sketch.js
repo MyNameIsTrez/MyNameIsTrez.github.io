58fbf6a3d099c8fc716c8c40bf001cf563e21929
MyNameIsTrez <welfje@gmail.com>
2019-03-20 15:45:47 +0100

    Can't have duplicate words at the end of words[0] anymore.

--- projects/wordlist_learning_tool/sketch.js
+++ projects/wordlist_learning_tool/sketch.js
@@ -1,10 +1,8 @@
 let wordsExist = false;
-let words = {
-  1: [],
-  2: [],
-  3: [],
-  4: []
-};
+let words = [
+  [],
+  []
+];
 let index = 0;
 let released = true;
 let showAnswer = true;
@@ -14,43 +12,43 @@ function setup() {
   google.charts.load('current', {
     packages: ['corechart']
   });
-  google.charts.setOnLoadCallback(getData);
+  google.charts.setOnLoadCallback(sendQuery);
 }
 
-function getData() {
+function sendQuery() {
   // let spreadsheetFilter = 'select%20B%2C%20offset%201' // select B, offset 1
-
   let spreadsheetFilter = 'select%20B' // select B
   let query = new google.visualization.Query(spreadsheetURL + '/gviz/tq?tq=' + spreadsheetFilter);
-  query.send(handleQueryResponse1);
+  query.send(queryToData1);
+}
+
+function queryToData1(response) {
+  const data = response.getDataTable();
+  dataToWords1(data);
+}
+
+function dataToWords1(data) {
+  for (let i = 0; i < data.wg.length; i++) {
+    words[0].push([]);
+    words[0][i][0] = data.wg[i].c[0].v;
+  }
+
+  document.getElementById('words1').innerHTML = words[0][index];
+  document.getElementById('progress').innerHTML = `${index + 1}/${words[0].length}`;
 
   spreadsheetFilter = 'select%20D' // select D
   query = new google.visualization.Query(spreadsheetURL + '/gviz/tq?tq=' + spreadsheetFilter);
-  query.send(handleQueryResponse2);
+  query.send(queryToData2);
 }
 
-function handleQueryResponse1(response) {
+function queryToData2(response) {
   const data = response.getDataTable();
-  dataIntoWordArray1(data);
+  dataToWords2(data);
 }
 
-function handleQueryResponse2(response) {
-  const data = response.getDataTable();
-  dataIntoWordArray2(data);
-}
-
-function dataIntoWordArray1(data) {
+function dataToWords2(data) {
   for (let i = 0; i < data.wg.length; i++) {
-    words[1][i] = data.wg[i].c[0].v;
-  }
-
-  document.getElementById('words1').innerHTML = words[1][index];
-  document.getElementById('progress').innerHTML = `${(index + 1)}/${words[1].length}`;
-}
-
-function dataIntoWordArray2(data) {
-  for (let i = 0; i < data.wg.length; i++) {
-    words[2][i] = data.wg[i].c[0].v;
+    words[0][i][1] = data.wg[i].c[0].v;
   }
 
   wordsExist = true;
@@ -70,7 +68,7 @@ function mousePressed() {
     }
     released = false;
 
-    if (index >= words[1].length) {
+    if (index >= words[0].length) {
       document.getElementById("words1").innerHTML = 'You\'re done!';
       document.getElementById("words2").innerHTML = '';
       done = true;
@@ -81,29 +79,26 @@ function mousePressed() {
     switch (mouseButton) {
       case RIGHT:
         if (!showAnswer) {
-          words[1].push(words[1][index - 1]);
-          words[2].push(words[2][index - 1]);
+          // if the word hasn't been added to the end of words[0] yet, do so
+          if (!(words[0][index - 1][0] === words[0][words[0].length - 1][0])) {
+            words[0].push([words[0][index - 1][0], words[0][index - 1][1]]);
+            words[1].push([words[0][index - 1][0], words[0][index - 1][1]]);
+          }
 
-          words[3].push(words[1][index - 1]);
-          words[4].push(words[2][index - 1]);
-
-          document.getElementById("words1").innerHTML = words[1][index];
+          document.getElementById("words1").innerHTML = words[0][index][0];
           document.getElementById("words2").innerHTML = '';
 
-          let progress = `${(index + 1)}/${words[1].length}`;
+          let progress = `${index + 1}/${words[0].length}`;
           document.getElementById("progress").innerHTML = progress;
 
           showAnswer = true;
         } else {
-          words[1].push(words[1][index]);
-          words[2].push(words[2][index]);
+          words[0].push([words[0][index][0], words[0][index][1]]);
+          words[1].push([words[0][index][0], words[0][index][1]]);
 
-          words[3].push(words[1][index]);
-          words[4].push(words[2][index]);
+          document.getElementById("words2").innerHTML = words[0][index][1];
 
-          document.getElementById("words2").innerHTML = words[2][index];
-
-          let progress = `${(index + 1)}/${words[1].length}`;
+          let progress = `${index + 1}/${words[0].length}`;
           document.getElementById("progress").innerHTML = progress;
 
           index++;
@@ -112,15 +107,15 @@ function mousePressed() {
         break;
       case LEFT:
         if (!showAnswer) {
-          document.getElementById("words1").innerHTML = words[1][index];
+          document.getElementById("words1").innerHTML = words[0][index][0];
           document.getElementById("words2").innerHTML = '';
 
-          let progress = `${(index + 1)}/${words[1].length}`;
+          let progress = `${index + 1}/${words[0].length}`;
           document.getElementById("progress").innerHTML = progress;
 
           showAnswer = true;
         } else {
-          document.getElementById("words2").innerHTML = words[2][index];
+          document.getElementById("words2").innerHTML = words[0][index][1];
 
           index++;
           showAnswer = false;
