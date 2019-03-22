0abca47d38a18a94b25ac49c896f6ed64ede9e27
MyNameIsTrez <welfje@gmail.com>
2019-03-22 22:55:51 +0100

    Made the text smaller: 'Click once before this text disappears on mobile!'

--- projects/wordlist_learning_tool/app.js
+++ projects/wordlist_learning_tool/app.js
@@ -12,8 +12,6 @@ let oneDirection;
 
 function setup() {
   oneDirection = document.querySelectorAll('oneDirection').checked; // whether the words get asked in one direction
-
-  document.getElementById("words1").innerHTML = 'Click once before this disappears on mobile!';
   google.charts.load('current', {
     packages: ['corechart']
   });
