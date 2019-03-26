e1858662f26f9c2330d6a8be77a978e251cac90f
MyNameIsTrez <welfje@gmail.com>
2019-03-26 11:08:32 +0100

    Added to last update.

--- projects/game_of_life/js/sketchFunctions/scenes/settings/settingsScene.js
+++ projects/game_of_life/js/sketchFunctions/scenes/settings/settingsScene.js
@@ -1,12 +1,6 @@
 function settingsScene() {
   if (scene !== 'tutorial') {
     if (scene === "settings") {
-      for (let y in cells) { // resets all the cells their ticksDead counter
-        for (let x in cells[y]) {
-          cells[y][x].ticksDead = maxTicksColored + 1;
-        }
-      }
-
       scene = "game";
     } else {
       scene = "settings";
--- projects/game_of_life/js/sketchFunctions/scenes/tutorialScene.js
+++ projects/game_of_life/js/sketchFunctions/scenes/tutorialScene.js
@@ -1,11 +1,5 @@
 function tutorialScene() {
   if (scene === 'tutorial') {
-    for (let y in cells) { // resets all the cells their ticksDead counter
-      for (let x in cells[y]) {
-        cells[y][x].ticksDead = maxTicksColored + 1;
-      }
-    }
-
     scene = "game";
   } else {
     scene = 'tutorial';
