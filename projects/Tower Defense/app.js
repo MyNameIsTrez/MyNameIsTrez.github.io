93e1136de1f47a90d40afa7f1ca531ee085b0802
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-04-01 17:40:59 +0200

    Updated code.

--- projects/Tower Defense/app.js
+++ projects/Tower Defense/app.js
@@ -1,26 +1,58 @@
 const cellsHorizontal = 40;
 const cellsVertical = 20;
 const cellWidthHeight = 20;
-let cells = [];
+const width = cellsHorizontal * cellWidthHeight;
+const height = cellsVertical * cellWidthHeight;
+const cells = [];
 
 function setup() {
-  background(0);
-  stroke(255);
+  createCanvas(width + 1, height + 1);
   for (let y = 0; y < cellsVertical; y++) {
+    cells.push([]);
     for (let x = 0; x < cellsHorizontal; x++) {
-      cells[y][x] = new Cell(x, y);
-      cells[y][x].show();
+      cells[y].push(new Cell(x, y, 'terrain'));
+    }
+  }
+}
+
+function draw() {
+  for (const row of cells) {
+    for (const cell of row) {
+      cell.show();
     }
   }
 }
 
 class Cell {
-  constructor(x, y) {
+  constructor(x, y, id) {
     this.x = x;
     this.y = y;
+    this.id = id;
   }
 
   show() {
-    rect(this.x, this.y, cellWidthHeight, cellWidthHeight);
+    push();
+    switch (this.id) {
+      case 'terrain':
+        fill(0);
+        break;
+      case 'wall':
+        fill(255);
+        break;
+    }
+    rect(this.x * cellWidthHeight, this.y * cellWidthHeight, cellWidthHeight, cellWidthHeight);
+    pop();
+  }
+}
+
+function mousePressed() {
+  switch (mouseButton) {
+    case LEFT:
+      if (mouseY > 0 && mouseX > 0 && mouseY < height && mouseX < width) {
+        const y = floor(mouseY / cellWidthHeight);
+        const x = floor(mouseX / cellWidthHeight);
+        cells[y][x].id = selectedID;
+      }
+      break;
   }
 }
