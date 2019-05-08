ccecce3dec8d798539e889185e6c8fd5c27b5ff2
MyNameIsTrez <welfje@gmail.com>
2019-05-08 17:45:03 +0200

    Worked on the coding challenge.

---
+++ projects/Coding Challenges/145 - 2D Ray Casting/classes/ray.js
@@ -0,0 +1,14 @@
+class Ray {
+	constructor(x, y) {
+		this.pos = createVector(x, y);
+		this.dir = createVector(1, 0);
+	}
+
+	show() {
+		push();
+		stroke(255);
+		translate(this.pos.x, this.pos.y);
+		line(0, 0, this.dir.x * 10, this.dir.y * 10);
+		pop();
+	}
+}
