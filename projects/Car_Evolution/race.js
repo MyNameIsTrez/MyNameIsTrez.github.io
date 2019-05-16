e745cd6372e7168967751bb64643c3df7ac0d5b9
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-05-16 23:32:10 +0200

    Moved the stats to the bottom-left.

--- projects/Car_Evolution/race.js
+++ projects/Car_Evolution/race.js
@@ -26,16 +26,16 @@ function raceUpdate() {
   fill(255);
   textSize(40);
 
-  text("generation: " + generation, 20, 50);
+  text("generation: " + generation, 50, height - 50);
   // text("laps: " + car.laps + ", score: " + car.score + "/" + car.checkpointCount, 20, 100);
 
   time = performance.now() - startTime; // The time from the beginning of the generation in ms.
-  text("time: " + (time / 1000).toFixed(2) + " sec", width / 2, 50);
-  if (recordTime)
-    text("record time: " + (recordTime / 1000).toFixed(2) + " sec", width / 2, 100);
-  else
-    text("record time: " + (time / 1000).toFixed(2) + " sec", width / 2, 100);
-  text("fps: " + frameRate().toFixed(0), width - 140, 50);
+  text("time: " + (time / 1000).toFixed(2) + " sec", 50, height - 100);
+  // if (recordTime)
+  //   text("record time: " + (recordTime / 1000).toFixed(2) + " sec", 50, height - 50);
+  // else
+  //   text("record time: " + (time / 1000).toFixed(2) + " sec", 50, height - 50);
+  text("fps: " + frameRate().toFixed(0), 50, height - 150);
   pop();
 }
 
