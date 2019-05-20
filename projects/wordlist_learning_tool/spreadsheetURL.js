90112b4b5112b2a67ce625bb89217b8c33256584
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-05-20 20:38:16 +0200

    Gebruikt nu de spreadsheet van woordenlijst Leiden 6-10.

--- projects/wordlist_learning_tool/spreadsheetURL.js
+++ projects/wordlist_learning_tool/spreadsheetURL.js
@@ -1,11 +1,14 @@
-// German Leiden 1-5
-const spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1Xdys1p6EMqCPbe607e40mHVTkEI1Ywhd-bqCNTOmfN0';
+// Woordenlijst Leiden 1-5
+// const spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1Xdys1p6EMqCPbe607e40mHVTkEI1Ywhd-bqCNTOmfN0';
 
-// German Leiden 1-10
+// Woordenlijst Leiden 6-10
+const spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1IekIuoX01xHyCHSuszLTIyUObgYzSpaeSXKGfIwLZT0';
+
+// Woordenlijst Leiden 1-10
 // const spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1mzS7qgvcLEctfhm4p8welUXpeE9DyKjBFz14ZvYG_30';
 
-// English 5V P2 toetsweek
+// Engels 5V P2 toetsweek
 // const spreadsheetURL = 'https://docs.google.com/a/google.com/spreadsheets/d/1t3TuSANJlhotUmUpOIBQNYyZMU6rmNSAOge-KkssOpQ';
 
-// Testlist Words
+// Testing list with only 2 words
 // const spreadsheetURL = 'https://docs.google.com/spreadsheets/u/1/d/1IHRxr7bbprRPUg0gv1FHGylPHxS8uuRmrmEXatXyiEk';
