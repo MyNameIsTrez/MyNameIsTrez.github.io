ffd034a67001289a6984b77139b2ebfa4a33e02d
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-05-29 18:13:49 +0200

    Added 16 wool .pngs. Isometric view is now toggleable.

--- projects/minecraft_wall/app.js
+++ projects/minecraft_wall/app.js
@@ -1,5 +1,6 @@
 // Editable.
 let maxPosXYCount = 32;
+let isometricView = true;
 let shadows = true;
 
 // TODO
@@ -27,28 +28,29 @@ let shadows = true;
 
 // https://minecraft.gamepedia.com/Wool
 let colors = [
-  [221, 221, 221], // '#DDDDDD'
-  [219, 125, 62], // 'DB7D3E'
-  [179, 80, 188], // '#B350BC'
-  [107, 138, 201], // '#6B8AC9'
-  [177, 166, 39], // '#B1A627'
-  [65, 174, 56], // '#41AE38'
-  [208, 132, 153], // '#D08499'
-  [64, 64, 64], // '#404040'
-  [154, 161, 161], // '#9AA1A1'
-  [46, 110, 137], // '#2E6E89'
-  [126, 61, 181], // '#7E3DB5'
-  [46, 56, 141], // '#2E388D'
-  [79, 50, 31], // '#4F321F'
-  [53, 70, 27], // '#35461B'
-  [150, 52, 48], // '#963430'
-  [25, 22, 22] // '#191616'
+  ['white', [221, 221, 221]],
+  ['orange', [219, 125, 62]],
+  ['magenta', [179, 80, 188]],
+  ['light_blue', [107, 138, 201]],
+  ['yellow', [177, 166, 39]],
+  ['lime', [65, 174, 56]],
+  ['pink', [208, 132, 153]],
+  ['gray', [64, 64, 64]],
+  ['light_gray', [154, 161, 161]],
+  ['cyan', [46, 110, 137]],
+  ['purple', [126, 61, 181]],
+  ['blue', [46, 56, 141]],
+  ['brown', [79, 50, 31]],
+  ['green', [53, 70, 27]],
+  ['red', [150, 52, 48]],
+  ['black', [25, 22, 22]]
 ];
 
-let wool;
-
+let woolTextures = [];
 function preload() {
-  wool = loadImage('wool.png');
+  for (color of colors) {
+    woolTextures.push(loadImage('wool_textures/' + color[0] + '.png'));
+  }
 }
 
 // Not editable; just making these variables global.
@@ -56,7 +58,8 @@ let walls;
 let sliderPosXCount, sliderPosYCount;
 let sliderBackgroundColor, sliderCursorColor;
 let cursorIndex = 0;
-let cursorColor = colors[cursorIndex];
+let cursorColor = colors[cursorIndex][1];
+
 let wallSize;
 if (innerWidth < innerHeight) {
   wallSize = Math.trunc((innerWidth * 0.75) / maxPosXYCount);
@@ -66,6 +69,9 @@ if (innerWidth < innerHeight) {
 
 function setup() {
   sliderBackgroundColor = createColorPicker('#7ab7fa').input(update); // light blue
+  createButton('isometric view').mousePressed(function () {
+    isometricView = !isometricView;
+  });
   createButton('shadows').mousePressed(function () {
     shadows = !shadows;
   });
@@ -97,7 +103,7 @@ function draw() {
 
 function update() {
   background(sliderBackgroundColor.color().levels);
-  if (shadows) {
+  if (isometricView && shadows) {
     for (const column of walls) {
       for (const wall of column) {
         if (wall && !Array.isArray(wall)) {
@@ -121,20 +127,22 @@ function createWallsArray() {
   walls = [];
   for (let posX = 0; posX < maxPosXYCount; posX++) {
     walls.push([]);
-    // for (let posY = 0; posY < maxPosXYCount; posY++) {
-    //   walls[posX].push([]);
-    // }
   }
 }
 
 
 function mouseAction() {
   const posX = floor((mouseX - wallSize) / wallSize);
-  const posY = floor((mouseY - wallSize) / wallSize);
+  let posY;
+  if (isometricView) {
+    posY = floor((mouseY - wallSize) / wallSize);
+  } else {
+    posY = floor((mouseY - 2 * wallSize) / wallSize);
+  }
 
   if (mouseButton === LEFT) {
     if (walls[posX][posY] && !Array.isArray(walls[posX][posY])) {
-      walls[posX][posY].rgb = cursorColor;
+      walls[posX][posY].colorIndex = cursorIndex;
     } else {
       walls[posX][posY] = new Wall(posX, posY, wallSize, cursorIndex);
     }
@@ -167,67 +175,67 @@ function keyPressed() {
   switch (key) {
     case '1':
       cursorIndex = 0;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '2':
       cursorIndex = 1;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '3':
       cursorIndex = 2;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '4':
       cursorIndex = 3;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '5':
       cursorIndex = 4;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '6':
       cursorIndex = 5;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '7':
       cursorIndex = 6;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '8':
       cursorIndex = 7;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '9':
       cursorIndex = 8;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '0':
       cursorIndex = 9;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '!':
       cursorIndex = 10;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '@':
       cursorIndex = 11;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '#':
       cursorIndex = 12;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '$':
       cursorIndex = 13;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
     case '%':
       cursorIndex = 14;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
-    case '^': // Dead
+    case '^':
       cursorIndex = 15;
-      cursorColor = colors[cursorIndex];
+      cursorColor = colors[cursorIndex][1];
       break;
   }
 }
@@ -239,7 +247,6 @@ class Wall {
     this.posY = posY;
     this.size = size;
     this.colorIndex = colorIndex;
-    this.rgb = colors[colorIndex];
 
     this.x = posX * size + size;
     this.y = posY * size + size * 2;
@@ -248,15 +255,13 @@ class Wall {
   draw() {
     push();
     noStroke();
-
-    // Side (real tile).
-    image(wool, this.x, this.y, this.size, this.size);
-    fill(this.rgb[0] / 4 * 3, this.rgb[1] / 4 * 3, this.rgb[2] / 4 * 3, 256 / 4 * 2.5);
-    rect(this.x, this.y, this.size, this.size);
-    // Top.
-    image(wool, this.x, this.y - this.size, this.size, this.size);
-    fill(this.rgb[0], this.rgb[1], this.rgb[2], 256 / 4 * 2.5);
-    rect(this.x, this.y - this.size, this.size, this.size);
+    if (isometricView) {
+      // Top. (where you click in isometric view)
+      image(woolTextures[this.colorIndex], this.x, this.y - this.size, this.size, this.size);
+    }
+    // Side. (actual tile position where you click in 2D view)
+    tint(127);
+    image(woolTextures[this.colorIndex], this.x, this.y, this.size, this.size);
     pop();
   }
 
@@ -264,7 +269,7 @@ class Wall {
     push();
     noStroke();
     // Shadow triangle below.
-    fill(this.rgb[0] * 0.25, this.rgb[1] * 0.25, this.rgb[2] * 0.25);
+    fill(colors[this.colorIndex][1][0] * 0.25, colors[this.colorIndex][1][1] * 0.25, colors[this.colorIndex][1][2] * 0.25);
     triangle(this.x, this.y + this.size, this.x + this.size, this.y + this.size, this.x + this.size, this.y + this.size * 1.75);
     // Shadow rectangle below to the right.
     rect(this.x + this.size, this.y + this.size, this.size, this.size * 0.75);
