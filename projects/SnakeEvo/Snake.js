bca32f1d7e446aa5e2b53733a0ac30f09d55cfa7
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-07-15 05:00:07 +0200

    Added SnakeEvo.

---
+++ projects/SnakeEvo/Snake.js
@@ -0,0 +1,187 @@
+class Snake {
+  constructor(index, brain) {
+    this.index = index;
+
+    this.agentX = index % agentsHor;
+    this.agentY = floor(index / agentsVer);
+
+    this.body = [];
+    this.body[0] = this.newGenPos();
+    this.xDir = 0;
+    this.yDir = 1;
+    this.alive = true;
+    this.pxls = [];
+
+    // Neuroevolution.
+    this.score = 0; // Maybe this should be 1?
+    this.fitness = 0;
+    if (brain) {
+      this.brain = brain.copy();
+    } else {
+      // The 'pixels' (same as w*h) coming from pxls.
+      const inputs = w * h;
+      // North, east, south, west.
+      const outputs = 4;
+      // ceil((#inputs + #outputs) / 2).
+      const hiddenLayers = ceil((inputs + outputs) / 2);
+
+      this.brain = new NeuralNetwork(inputs, hiddenLayers, outputs);
+    }
+  }
+
+  update() {
+    const head = this.body[this.body.length - 1].copy();
+    this.body.shift();
+    this.think();
+    head.x += this.xDir;
+    head.y += this.yDir;
+    this.body.push(head);
+  }
+
+  draw() {
+    push();
+    noStroke();
+    for (let i = 0; i < this.body.length; i++) {
+      if (debugColors) {
+        if (i === 0) {
+          fill(255, 200, 0, 150);
+        } else if (i === this.body.length - 1) {
+          fill(0, 0, 255, 150);
+        } else {
+          fill(255, 150);
+        }
+      } else {
+        fill(255);
+      }
+      const x = this.body[i].x * scl + this.agentX * agentWidth;
+      const y = this.body[i].y * scl + this.agentY * agentHeight;
+      square(x, y, scl);
+    }
+    pop();
+  }
+
+  die() {
+    // savedSnakes.push(this); // Shouldn't this be where this line goes, instead of below?
+    this.alive = false;
+    savedSnakes.push(this);
+  }
+
+  think() {
+    let inputs = [];
+
+    for (let x = 0; x < w; x++) {
+      for (let y = 0; y < h; y++) {
+        // Should I map the answer between 0 and 1?
+        // Should I have the head be a different input number than the body?
+        inputs.push(this.pxls[x][y]);
+      }
+    }
+
+    let output = this.brain.predict(inputs);
+
+    // Is this the best way to do this?
+    let i = output.indexOf(max(output));
+    switch (i) {
+      case 0:
+        // Go north.
+        this.setDir(0, -1);
+        break;
+      case 1:
+        // Go east.
+        this.setDir(1, 0);
+        break;
+      case 2:
+        // Go south.
+        this.setDir(0, 1);
+        break;
+      case 3:
+        // Go west.
+        this.setDir(-1, 0);
+        break;
+    }
+  }
+  
+  mutate() {
+    this.brain.mutate(mutationRate);
+  }
+
+  emptyPxls() {
+    this.pxls = [];
+    for (let x = 0; x < w; x++) {
+      this.pxls.push([]);
+      for (let y = 0; y < h; y++) {
+        this.pxls[x][y] = 0;
+      }
+    }
+  }
+
+  addSnakePxls() {
+    for (let j = 0; j < this.body.length; j++) {
+      const x = this.body[j].x;
+      const y = this.body[j].y;
+      this.pxls[x][y] = 1;
+    }
+  }
+
+  addFoodPxls() {
+    const x = this.food.pos.x;
+    const y = this.food.pos.y;
+    this.pxls[x][y] = 2;
+  }
+
+  drawBorder() {
+    const agentX = this.index % agentsHor;
+    const agentY = floor(this.index / agentsVer);
+
+    push();
+    noFill();
+    strokeWeight(3);
+    stroke(180);
+    rect(agentX * agentWidth, agentY * agentHeight, agentWidth, agentHeight);
+    pop();
+  }
+
+  setDir(x, y) {
+    this.xDir = x;
+    this.yDir = y;
+  }
+
+  eat(pos) {
+    const x = this.body[this.body.length - 1].x;
+    const y = this.body[this.body.length - 1].y;
+    if (x === pos.x && y === pos.y) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  newGenPos() {
+    const x = floor(w / 2);
+    const y = floor(h / 2);
+    const pos = createVector(x, y);
+    return pos;
+  }
+
+  grow() {
+    const head = this.body[this.body.length - 1].copy();
+    this.body.push(head);
+  }
+
+  checkDeath() {
+    const x = this.body[this.body.length - 1].x;
+    const y = this.body[this.body.length - 1].y;
+    // If the snake is offscreen.
+    if (x < 0 || x >= w || y < 0 || y >= h) {
+      return true;
+    }
+    // If the snake is eating itself.
+    for (let i = 0; i < this.body.length - 1; i++) {
+      const part = this.body[i];
+      if (part.x === x && part.y === y) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
---
+++ projects/SnakeEvo/app.js
@@ -0,0 +1,123 @@
+let snakes = [];
+let foods = [];
+
+const width = innerWidth - 5; // The Width that can be used for drawing this program.
+const height = innerHeight - 6; // The height that can be used for drawing this program.
+const w = 9; // How many 'pxls' there are horizontally per snake game.
+const h = 9; // How many 'pxls' there are horizontally per snake game.
+const agentsHor = 10; // How many snakes there are horizontally.
+const agentsVer = 10; // How many snakes there are vertically.
+const mutationRate = 0.1; // How much snakes will mutate after each generation.
+const debugColors = false; // Colors the head, body and tail of each snake differently.
+const maxTicksWithoutFood = 10; // The maximum amount of ticks that a snake can go without food before dying.
+
+const agentsCount = agentsHor * agentsVer;
+const maxSclWidth = width / w / agentsHor;
+const maxSclHeight = height / h / agentsVer;
+const scl = maxSclWidth < maxSclHeight ? maxSclWidth : maxSclHeight;
+const agentWidth = scl * w;
+const agentHeight = scl * h;
+const finalWidth = agentWidth * agentsHor;
+const finalHeight = agentHeight * agentsVer;
+let generation = 1;
+
+function setup() {
+  createCanvas(finalWidth, finalHeight);
+
+  background(0);
+  for (let i = 0; i < agentsCount; i++) {
+    const snake = new Snake(i);
+    snakes[i] = snake;
+    snake.emptyPxls();
+    snake.addSnakePxls();
+    snake.food = new Food(i);
+    snake.addFoodPxls();
+    // snakes[i] = snake; // I don't think this line is necessary, because 'snakes' just stores a pointer to the snake object, and doesn't copy the snake object. We can thus add 'food' after it has been added to 'snakes'.
+  }
+
+  for (const snake of snakes) {
+    snake.draw();
+    snake.food.draw();
+    snake.drawBorder();
+  }
+
+  drawGenCount();
+}
+
+function draw() {
+  if (frameCount % 1 === 0) { // Slows the game down.
+    background(0);
+    for (const snake of snakes) {
+      if (snake.alive === true) {
+        snake.emptyPxls();
+        snake.addSnakePxls();
+        snake.addFoodPxls();
+
+        snake.update();
+
+        // if (snake.index === 0) {
+        //   console.log(snake.pxls, snake.previousPxls, snake.secondPreviousPxls);
+        // }
+        if (snake.checkDeath() || equalArrays(snake.pxls, snake.secondPreviousPxls) || equalArrays(snake.pxls, snake.previousPxls)) {
+          snake.die();
+        }
+
+        snake.secondPreviousPxls = snake.previousPxls;
+        snake.previousPxls = snake.pxls;
+
+        if (snake.eat(snake.food.pos)) {
+          snake.grow();
+          snake.score++;
+          snake.food.setNewPos();
+        }
+
+        snake.draw();
+      }
+      snake.food.draw();
+      snake.drawBorder();
+    }
+
+    drawGenCount();
+
+    // Check if all snakes are dead. If so, we go to the next generation.
+    let allSnakesDead = true;
+    for (const snake of snakes) {
+      if (snake.alive) {
+        allSnakesDead = false;
+      }
+    }
+    if (allSnakesDead) {
+      nextGeneration();
+    }
+  }
+}
+
+function equalArrays(array1, array2) {
+  if ((array1 && array2) || (array1 === 0 && array2 === 0)) {
+    if (!Array.isArray(array1) && !Array.isArray(array2)) {
+      return array1 === array2;
+    }
+
+    if (array1.length !== array2.length) {
+      return false;
+    }
+
+    for (var i = 0, len = array1.length; i < len; i++) {
+      if (!equalArrays(array1[i], array2[i])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+function drawGenCount() {
+  push();
+  textSize(30);
+  stroke(0);
+  strokeWeight(3);
+  fill(150, 255, 0);
+  text(`Generation: ${generation}`, 10, 35);
+  pop();
+}
