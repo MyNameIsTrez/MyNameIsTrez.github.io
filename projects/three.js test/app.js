acde0fee6bca149ad93959700b3dc21fa636cdcb
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-08-27 21:29:30 +0200

    Cleaned code by separating it into functions.

--- projects/three.js test/app.js
+++ projects/three.js test/app.js
@@ -1,16 +1,29 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// create global variables
+let camera, scene, renderer;
+let cube;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// assign global variables a starting value
+const cameraPosZ = 5;
+const rendererSizeReduction = 20;
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
 
-camera.position.z = 5;
+function setup() {
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = cameraPosZ;
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth - rendererSizeReduction, window.innerHeight - rendererSizeReduction);
+  document.body.appendChild(renderer.domElement);
+}
+
+function addCube() {
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -18,4 +31,8 @@ function animate() {
   cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
+
+
+setup();
+addCube();
 animate();
--- projects/three.js test/libs/three.js
+++ projects/three.js test/libs/three.js
@@ -23116,7 +23116,7 @@
 
 	function WebGLRenderer( parameters ) {
 
-		console.log( 'THREE.WebGLRenderer', REVISION );
+		// console.log( 'THREE.WebGLRenderer', REVISION );
 
 		parameters = parameters || {};
 
