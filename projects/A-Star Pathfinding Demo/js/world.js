f7e09c2f736a3bb2c2fee48587b6e3a9c376d409
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-09-19 22:58:04 +0200

    Upped the amount of walls from 10% to 30%.

--- projects/A-Star Pathfinding Demo/js/world.js
+++ projects/A-Star Pathfinding Demo/js/world.js
@@ -3,7 +3,7 @@ function createWorldArray() {
   for (let col = 0; col < cols; col++) {
     world[col] = [];
     for (let row = 0; row < rows; row++) {
-      const wall = random(1) < 0.1
+      const wall = random(1) < 0.3
       world[col][row] = new Tile(col, row, wall);
     }
   }
