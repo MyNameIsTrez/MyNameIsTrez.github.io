5e57e77cef89ce62a05b2ecc9c0fa5a0d29a02a2
MyNameIsTrez <32989873+MyNameIsTrez@users.noreply.github.com>
2019-10-15 10:03:14 +0200

    Tried adding a new spreadsheet, but it won't get loaded. Will have to look into this later when I have the time for it.

--- projects/wordlist_learning_tool/spreadsheetURL.js
+++ projects/wordlist_learning_tool/spreadsheetURL.js
@@ -1,14 +1,18 @@
-// Woordenlijst Leiden 1-5
+// Duits - Woordenlijst Leiden 1-5
 // const spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1Xdys1p6EMqCPbe607e40mHVTkEI1Ywhd-bqCNTOmfN0';
 
-// Woordenlijst Leiden 6-10
+// Duits - Woordenlijst Leiden 6-10
 // const spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1IekIuoX01xHyCHSuszLTIyUObgYzSpaeSXKGfIwLZT0';
 
-// Woordenlijst Leiden 1-10
+// Duits - Woordenlijst Leiden 1-10
 const spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1mzS7qgvcLEctfhm4p8welUXpeE9DyKjBFz14ZvYG_30';
 
-// Engels 5V P2 toetsweek
+// Engels - 5V P2 toetsweek
 // const spreadsheetURL = 'https://docs.google.com/a/google.com/spreadsheets/d/1t3TuSANJlhotUmUpOIBQNYyZMU6rmNSAOge-KkssOpQ';
 
-// Testing list with only 2 words
+// Testing
 // const spreadsheetURL = 'https://docs.google.com/spreadsheets/u/1/d/1IHRxr7bbprRPUg0gv1FHGylPHxS8uuRmrmEXatXyiEk';
+
+// Duits - Examenidioom pagina 59-61
+// Why doesn't this work?
+// const spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1D_4wk6K_qizDLikEaSa8C6WfHZZapGVDhQac6ykPfb0';
