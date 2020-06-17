e7663f94aa0dbf8108eb8f7cb308518bbfb18dfe
MyNameIsTrez <welfje@gmail.com>
2020-06-17 06:23:48 +0200

    Got visualisation for corona statistics working.

--- projects/corona-map/sketch.js
+++ projects/corona-map/sketch.js
@@ -1,7 +1,14 @@
-let youtubeData;
+// covid-data.csv: https://ourworldindata.org/coronavirus-source-data
+
+// Customizable.
+const topic1 = "total_cases";
+
+
+// Not customizable.
+let covidData;
 let countries;
 
-const mappa = new Mappa('Leaflet');
+const mappa = new Mappa("Leaflet");
 let trainMap;
 let canvas;
 
@@ -11,42 +18,76 @@ const options = {
 	lat: 0,
 	lng: 0,
 	zoom: 1.5,
-	style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+	style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
 };
 
+const unusableTopics = [
+	"iso_code", "continent", "location", "date"
+];
+
 function preload() {
-	youtubeData = loadTable('subscribers_geo.csv', 'header');
-	countries = loadJSON('countries.json');
+	covidData = loadTable("covid_data.csv", "header");
+	countries = loadTable("lat_lon_data.csv", "header");
 }
 
 function setup() {
 	canvas = createCanvas(800, 600);
+
 	trainMap = mappa.tileMap(options);
 	trainMap.overlay(canvas);
 
-	let maxSubs = 0;
-	let minSubs = Infinity;
+	console.log(covidData);
+	// console.log(countries);
 
-	for (const row of youtubeData.rows) {
-		const country = row.get('country_id').toLowerCase();
-		const latlon = countries[country];
+	topics = covidData.columns.filter(function (topic) {
+		return !unusableTopics.includes(topic);
+	});
+
+	let previousCountryName;
+	for (const rowCovidData of covidData.rows) {
+		const countryName = rowCovidData.get("location");
+
+		if (previousCountryName === countryName) {
+			data[data.length - 1].count += Number(rowCovidData.get(topic1));
+			continue;
+		} else {
+			previousCountryName = countryName;
+		}
+
+		const isoCode = rowCovidData.get("iso_code");
+
+		let latlon;
+		for (const row2 of countries.rows) {
+			if (isoCode === row2.get("Alpha-3 code").slice(2, -1)) {
+				latlon = {
+					lat: Number(row2.get("Latitude (average)").slice(2, -1)),
+					lon: Number(row2.get("Longitude (average)").slice(2, -1))
+				};
+				break;
+			}
+		}
+
 		if (latlon) {
-			const lat = latlon[0];
-			const lon = latlon[1];
-			const count = Number(row.get('subscribers'));
+			const lat = latlon.lat;
+			const lon = latlon.lon;
+			const count = Number(rowCovidData.get(topic1));
 			data.push({
 				lat,
 				lon,
 				count
 			});
-			maxSubs = max(maxSubs, count);
-			minSubs = min(maxSubs, count);
 		}
 	}
 
-	const minD = sqrt(minSubs);
-	const maxD = sqrt(maxSubs);
+	let minCount = Infinity;
+	let maxCount = 0;
+	for (const country of data) {
+		minCount = min(minCount, country.count);
+		maxCount = max(maxCount, country.count);
+	}
 
+	const minD = sqrt(max(minCount, 0)); // sqrt() can't accept negative numbers.
+	const maxD = sqrt(maxCount);
 	for (const country of data) {
 		country.diameter = map(sqrt(country.count), minD, maxD, 1, 20);
 	}
@@ -56,7 +97,8 @@ function draw() {
 	clear();
 	for (const country of data) {
 		const pix = trainMap.latLngToPixel(country.lat, country.lon);
-		fill((sin(frameCount / 60 * TWO_PI / 4) + 1) / 2 * 255, 0, 200, 100);
+		const red = 255 * (sin(frameCount / 60 * TWO_PI / 4) + 1) / 2;
+		fill(red, 0, 200, 100);
 		const zoom = trainMap.zoom();
 		const scl = pow(2, zoom);
 		ellipse(pix.x, pix.y, country.diameter * scl);
