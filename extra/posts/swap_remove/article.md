f24eea6ccf8713484704eed33e857b442b7b09a5
MyNameIsTrez <welfje@gmail.com>
2024-03-01 00:56:25 +0100

    Formatting

--- extra/posts/swap_remove/article.md
+++ extra/posts/swap_remove/article.md
@@ -2,13 +2,13 @@
 
 - Mention that C++ is used for the sake of comparing with set() and unordered_set()
 - Mention that rand() is used which returns an int, which limits the area somewhat that depends on the size of an int on your system
-- Using -O3
+- Using -O3 and -march=native
 - Note that comparing times with the `time` command isn't going to be very accurate, but it's convenient.
 - With both approaches filling the area is faster with a smaller radius.
 - With the naive approach it fills an area faster with a smaller radius, apart from r10k
 - With the swap-remove approach it often fills an area faster with a smaller radius
 - Make table that compares naive vs set() vs unordered_set() vs swap-remove
-- With the naive approach, the program is faster *with* the `!area[index]`
+- With the naive approach, the program is faster _with_ the `!area[index]`
 - `v2i` means value-to-index, and `i2v` means index-to-value
 - Mention at the end that simulated annealing would be a simple upgrade
 - Swap remove method doesn't need a 2D boolean array
@@ -16,10 +16,10 @@
 - TODO: Think of a name for swap_remove + yin-yang array pair Set technique
 - A swap-remove array is essentially an unordered set with integer keys.
 
-| w1k h1k i10 | r0   | r1   | r10  | r100 | r1k |
-|-------------|------|------|------|------|-----|
-| naive       | | | | | |
-| shuffle     | | | | | |
-| swap-remove | | | | | |
-| set | | | | | |
-| unordered_set | | | | | |
+| w1k h1k i10   | r0  | r1  | r10 | r100 | r1k |
+| ------------- | --- | --- | --- | ---- | --- |
+| naive         |     |     |     |      |     |
+| shuffle       |     |     |     |      |     |
+| swap-remove   |     |     |     |      |     |
+| set           |     |     |     |      |     |
+| unordered_set |     |     |     |      |     |
--- extra/posts/swap_remove/src/plot.py
+++ extra/posts/swap_remove/src/plot.py
@@ -1,51 +1,87 @@
 import itertools
-import matplotlib.pyplot as plt
 import multiprocessing
 import subprocess
+from pathlib import Path
+
+import matplotlib.pyplot as plt
 
 area_width = 1000
 area_height = 1000
 benchmark_iterations = 10
 
+
 def main():
-	plt.title("Circle packing algorithms")
-	plt.xlabel("Radius")
-	plt.ylabel("Milliseconds")
+    plt.title("Circle packing algorithms")
+    plt.xlabel("Radius")
+    plt.ylabel("Milliseconds")
 
-	radii = (0, 1, 10, 100, 1000)
+    radii = (0, 1, 10, 100, 1000)
 
-	algorithms = ("NAIVE", "SHUFFLE", "SWAP_REMOVE", "SET", "UNORDERED_SET")
+    algorithms = ("NAIVE", "SHUFFLE", "SWAP_REMOVE", "SET", "UNORDERED_SET")
 
-	product = list(itertools.product(algorithms, radii))
+    Path("bin").mkdir(exist_ok=True)
 
-	pool = multiprocessing.Pool(4)
+    pool = multiprocessing.Pool(4)
+    product = list(itertools.product(algorithms, radii))
+    pool.map(compile_test, product)
 
-	pool.map(compile_test, product)
+    for i, algorithm_timings in enumerate(batched(pool.map(run_test, product), 5)):
+        plt.plot(radii, algorithm_timings, label=algorithms[i])
 
-	for i, algorithm_timings in enumerate(batched(pool.map(run_test, product), 5)):
-		plt.plot(radii, algorithm_timings, label=algorithms[i])
+    plt.xscale("log")
+    # plt.yscale("log")
 
-	plt.xscale("log")
-	# plt.yscale("log")
+    plt.legend()  # Shows labels
+    # plt.show()
+    plt.savefig("plot.png")
 
-	plt.legend() # Shows labels
-	# plt.show()
-	plt.savefig("plot.png")
 
 def compile_test(tup):
-	algorithm, radius = tup
-	print(f"compiling {algorithm} with a radius of {radius}")
+    algorithm, radius = tup
+    print(f"compiling {algorithm} with a radius of {radius}")
 
-	subprocess.run(["c++", "main.cpp", "-Wall", "-Wextra", "-Werror", "-Wpedantic", "-Wshadow", "-Wfatal-errors", "-g", "-fsanitize=address,undefined", "-O3", "-std=c++20", f"-DALGORITHM={algorithm}", f"-DRADIUS={radius}", f"-DAREA_WIDTH={area_width}", f"-DAREA_HEIGHT={area_height}", "-DCIRCLE_PACKING", "-DPRINT_TIME_ONLY", f"-DBENCHMARK_ITERATIONS={benchmark_iterations}", f"-obin/{algorithm}_{radius}"])
+    completed_process = subprocess.run(
+        [
+            "c++",
+            "main.cpp",
+            "-Wall",
+            "-Wextra",
+            "-Werror",
+            "-Wpedantic",
+            "-Wshadow",
+            "-Wfatal-errors",
+            "-g",
+            # "-fsanitize=address,undefined",
+            "-O3",
+            "-march=native",
+            "-std=c++20",
+            f"-DALGORITHM={algorithm}",
+            f"-DRADIUS={radius}",
+            f"-DAREA_WIDTH={area_width}",
+            f"-DAREA_HEIGHT={area_height}",
+            "-DCIRCLE_PACKING",
+            "-DPRINT_TIME_ONLY",
+            f"-DBENCHMARK_ITERATIONS={benchmark_iterations}",
+            f"-obin/{algorithm}_{radius}",
+        ]
+    )
+    assert completed_process.returncode == 0
+
 
 def run_test(tup):
-	algorithm, radius = tup
-	print(f"running {algorithm} with a radius of {radius}")
-	return int(subprocess.run([f"bin/{algorithm}_{radius}"], capture_output=True).stdout)
+    algorithm, radius = tup
+    print(f"running {algorithm} with a radius of {radius}")
+    completed_process = subprocess.run(
+        [f"bin/{algorithm}_{radius}"], capture_output=True
+    )
+    assert completed_process.returncode == 0
+    return int(completed_process.stdout)
+
 
 def batched(lst, n):
     for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+        yield lst[i : i + n]
+
 
 if __name__ == "__main__":
-	main()
+    main()
