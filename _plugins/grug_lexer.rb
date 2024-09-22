68630c3fbf5ad96ba6ae3713364482d94f0c1661
Sander Bos <32989873+MyNameIsTrez@users.noreply.github.com>
2024-09-22 18:03:36 +0200

    Update grug_lexer.rb

--- _plugins/grug_lexer.rb
+++ _plugins/grug_lexer.rb
@@ -89,7 +89,8 @@ Jekyll::Hooks.register :site, :pre_render do |site|
       rule %r/\d+[lu]*/i, Num::Integer
       rule %r([~!%^&*+=\|?:<>/-]), Operator
       rule %r/[()\[\],.]/, Punctuation
-      rule %r/(?:and|or|not)\b/, Operator::Word
+      rule %r/(?:and|or|not)\b/, Keyword
+      # rule %r/(?:and|or|not)\b/, Operator::Word
       rule %r/(?:true|false|NULL)\b/, Name::Builtin
 
       rule id do |m|
